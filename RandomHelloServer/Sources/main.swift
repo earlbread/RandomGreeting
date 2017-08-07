e2081212c8840de37b164d9644b10a05e1a0ca06
Seunghun Lee <waydi1@gmail.com>
2017-08-07 19:16:35 +0900

    Add greetings

--- RandomHelloServer/Sources/main.swift
+++ RandomHelloServer/Sources/main.swift
@@ -8,6 +8,28 @@ router.get("/") {
     next()
 }
 
+let greetings: [String] = {
+    "Hello",
+    "안녕하세요",
+    "こんにちは",
+    "你好",
+    "Bonjour",
+    "Hola",
+    "Guten Tag",
+    "Xin chào",
+    "Здравствуйте",
+    "Bom dia",
+    "Сайн байна уу",
+    "halo",
+    "assalom allaykum",
+    "Mirёdita",
+    "नमस्ते , नमस्कार",
+    "Ciao",
+    "Bună ziua",
+    "صباح الخير",
+    "สวัสดี"
+}
+
 Kitura.addHTTPServer(onPort: 8080, with: router)
 
 Kitura.run()
