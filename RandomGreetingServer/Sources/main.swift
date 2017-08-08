ee8615871472da3f127635d0402d710883ed7913
Seunghun Lee <waydi1@gmail.com>
2017-08-08 12:19:27 +0900

    Add JSON Response /greeting

--- RandomGreetingServer/Sources/main.swift
+++ RandomGreetingServer/Sources/main.swift
@@ -1,14 +1,16 @@
 import Kitura
+import SwiftyJSON
+
+#if os(Linux)
+    import Glibc
+    srand(UInt32(time(nil)))
+#else
+    import Darwin.C
+#endif
 
 let router = Router()
 
-router.get("/") {
-    request, response, next in
-    response.send("Hello, World!")
-    next()
-}
-
-let greetings: [String] = {
+let greetings: [String] = [
     "Hello",
     "안녕하세요",
     "こんにちは",
@@ -28,6 +30,17 @@ let greetings: [String] = {
     "Bună ziua",
     "صباح الخير",
     "สวัสดี"
+]
+
+router.get("/greeting") {
+    request, response, next in
+#if os(Linux)
+    let idx = Int(random() % greetings.count)
+#else
+    let idx = Int(arc4random_uniform(UInt32(greetings.count)))
+#endif
+    response.send(json: JSON(["greeting": greetings[idx]]))
+    next()
 }
 
 Kitura.addHTTPServer(onPort: 8080, with: router)
