999feef6379e461fe51c055c915dfd2671522cb9
Seunghun Lee <waydi1@gmail.com>
2017-08-08 16:19:52 +0900

    Add RandomGreeting iOS client

---
+++ RandomGreeting-iOS/RandomGreeting/RandomGreeting/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  RandomGreeting
+//
+//  Created by Seunghun Lee on 2017-08-08.
+//  Copyright © 2017 Seunghun Lee. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
---
+++ RandomGreeting-iOS/RandomGreeting/RandomGreeting/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RandomGreeting
+//
+//  Created by Seunghun Lee on 2017-08-08.
+//  Copyright © 2017 Seunghun Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
