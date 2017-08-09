df0412db6c9fd830e0d180ebda9fc94d80bdb644
Seunghun Lee <waydi1@gmail.com>
2017-08-09 23:19:18 +0900

    Build UI

--- RandomGreeting-iOS/RandomGreeting/RandomGreeting/ViewController.swift
+++ RandomGreeting-iOS/RandomGreeting/RandomGreeting/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var greetingLabel: UILabel!
+  @IBOutlet weak var greetingButton: UIButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
