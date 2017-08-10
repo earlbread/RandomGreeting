b224a21b3e218d0f65c4d93abc32311b6376b636
Seunghun Lee <waydi1@gmail.com>
2017-08-10 09:39:17 +0900

    Get greeting from URL when button is tapped

--- RandomGreeting-iOS/RandomGreeting/RandomGreeting/ViewController.swift
+++ RandomGreeting-iOS/RandomGreeting/RandomGreeting/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
+let greetingEndpoint = "http://localhost:8080/greeting"
+
 class ViewController: UIViewController {
   @IBOutlet weak var greetingLabel: UILabel!
-  @IBOutlet weak var greetingButton: UIButton!
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  @IBAction func getGreeting(_ sender: UIButton) {
+    let task = URLSession.shared.dataTask(with: URL(string: greetingEndpoint)!) {
+      data, response, error in
+      if let error = error {
+        print(error)
+        return
+      }
+
+      if let data = data {
+        var json: [String:String]?
+        do {
+          json = try JSONSerialization.jsonObject(with: data) as? [String:String]
+        } catch {
+          print("Could not serialize \(data)")
+          return
+        }
+        DispatchQueue.main.async {
+          if let greeting = json?["greeting"] {
+            self.greetingLabel.text = greeting
+          }
+        }
+      }
+    }
+
+    task.resume()
   }
-
 }
 
